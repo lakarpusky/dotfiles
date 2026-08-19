1917810db238066c9cc48265408b09675e684ca1
G. Montero <gabo.montero89@gmail.com>
2026-08-19 00:36:48 -0500

    feat(nvim): switch markdown preview to markview.nvim

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -4,47 +4,17 @@
 -- Markdown preview in browser with live updates
 --
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  "OXY2DEV/markview.nvim",
   ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-  keys = {
-    {
-      "<leader>mp",
-      "<cmd>MarkdownPreviewToggle<CR>",
-      desc = "Markdown Preview",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    preview = {
+      enable = false, -- don't render on attach; manual only
+      icon_provider = "internal",
     },
   },
-  config = function()
-    vim.g.mkdp_auto_start = 0
-    vim.g.mkdp_auto_close = 0
-    vim.g.mkdp_refresh_slow = 0
-    vim.g.mkdp_command_for_global = 0
-    vim.g.mkdp_open_to_the_world = 0
-    vim.g.mkdp_open_ip = ""
-    vim.g.mkdp_browser = ""
-    vim.g.mkdp_echo_preview_url = 0
-    vim.g.mkdp_browserfunc = ""
-    vim.g.mkdp_preview_options = {
-      mkit = {},
-      katex = {},
-      uml = {},
-      maid = {},
-      disable_sync_scroll = 0,
-      sync_scroll_type = "middle",
-      hide_yaml_meta = 1,
-      sequence_diagrams = {},
-      flowchart_diagrams = {},
-      content_editable = false,
-      disable_filename = 0,
-      toc = {},
-    }
-    vim.g.mkdp_markdown_css = ""
-    vim.g.mkdp_highlight_css = ""
-    vim.g.mkdp_port = ""
-    vim.g.mkdp_page_title = "「${name}」"
-    vim.g.mkdp_theme = "dark"
-  end,
+  keys = {
+    { "<leader>mp", "<CMD>Markview Toggle<CR>", desc = "Markview: toggle preview (global)" },
+    -- or buffer-scoped: "<CMD>Markview toggle<CR>"
+  },
 }
